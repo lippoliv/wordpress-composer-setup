a6e1fed574b622a4f5a64adc7648fcda8990aad6
Oliver Lippert <oliver@lipperts-web.de>
2021-05-11 08:38:16 +0200

    migrate remote folders + use wordpress maintenance mode
    Signed-off-by: Oliver Lippert <oliver@lipperts-web.de>

--- migrate.php
+++ migrate.php
@@ -1,14 +1,43 @@
 <?php
+    function startMaintenanceMode() {
+        $maintenanceFileContent = '<?php $upgrading = ' . time() . '; ?>';
+        file_put_contents('.maintenance', $maintenanceFileContent);
+    }
+
+    function endMaintenanceMode() {
+        unlink('.maintenance');
+    }
+
+    function replaceFolder(
+        $name,
+        $prefix = 'wp-content'
+    ) {
+        $orgDir = "$prefix/$name";
+
+        $oldDir = "$orgDir-old";
+        $newDir = "$orgDir-new";
+
+        rename($orgDir, $oldDir);
+        rename($newDir, $orgDir);
+        passthru("rm -rf $oldDir");
+    }
+
     $responseCode = 400;
+    startMaintenanceMode();
 
     $zipArchive = new ZipArchive();
     if ($zipArchive->open('wordpress.zip')) {
         $zipArchive->extractTo('.');
         $zipArchive->close();
 
+        replaceFolder('muplugins');
+        replaceFolder('plugins');
+        replaceFolder('themes');
+
         $responseCode = 200;
     }
 
+    endMaintenanceMode();
     unlink('wordpress.zip');
     unlink(__FILE__);
 
